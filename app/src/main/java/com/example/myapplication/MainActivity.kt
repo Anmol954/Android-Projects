5abeb29a5da9610ad0bea5fb7f1166c0e550bd92
SANDWICH <anmolmadhav2004@gmail.com>
2024-10-17 13:40:59 +0530

    Portfolio

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +18,57 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val t: androidx.appcompat.widget.Toolbar = findViewById(R.id.menu)
         setSupportActionBar(t)
+        supportActionBar?.title = "Anmol Madhav"
+
+
+        //For Projects
+
+        val project = findViewById<ImageButton>(R.id.proj)
+        project.setOnClickListener {
+            intent = Intent(this, project::class.java)
+            startActivity(intent)
+        }
+
+        //For Certifications
+
+        val certificate = findViewById<ImageButton>(R.id.cert)
+        certificate.setOnClickListener {
+            intent = Intent(this, certificate::class.java)
+            startActivity(intent)
+        }
+
+        //For Photo Gallery
+
+        val photo = findViewById<ImageButton>(R.id.photog)
+        photo.setOnClickListener {
+            intent = Intent(this, photo_galary::class.java)
+            startActivity(intent)
+        }
+
+        //For Skills
+
+        val skill = findViewById<ImageButton>(R.id.skill)
+        skill.setOnClickListener {
+            intent = Intent(this, skill::class.java)
+            startActivity(intent)
+        }
+
+        //For Academic
+
+        val academic = findViewById<ImageButton>(R.id.acad)
+        academic.setOnClickListener {
+            intent = Intent(this, academics::class.java)
+            startActivity(intent)
+        }
+
+        //For Hobby
+
+        val hobby = findViewById<ImageButton>(R.id.hobb)
+        hobby.setOnClickListener {
+            intent = Intent(this, hobby::class.java)
+            startActivity(intent)
+        }
+
     }
 
     //Function used to inflate the menu bar
@@ -30,11 +83,11 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
            R.id.email -> {
-               Toast.makeText(this, "Email", Toast.LENGTH_SHORT).show()
+               Toast.makeText(this, "anmolmadhav2004@gamil.com", Toast.LENGTH_SHORT).show()
                true
                }
            R.id.contact -> {
-               Toast.makeText(this, "Contact", Toast.LENGTH_SHORT).show()
+               Toast.makeText(this, "+91 908460978", Toast.LENGTH_SHORT).show()
                true
                }
            R.id.feedback -> {
@@ -43,5 +96,6 @@ class MainActivity : AppCompatActivity() {
            }
             else -> super.onOptionsItemSelected(item)
         }
+
     }
 }
