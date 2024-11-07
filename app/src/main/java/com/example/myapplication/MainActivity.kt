59fd3a8f1bdcf1177e3b3429ecbae77f6013dd97
SANDWICH <anmolmadhav2004@gmail.com>
2024-11-07 10:21:48 +0530

    Portfolio

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.myapplication
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -68,7 +73,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    //Function used to inflate the menu bar
+//Function used to inflate the menu bar
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
@@ -79,20 +84,42 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-           R.id.email -> {
-               Toast.makeText(this, "anmolmadhav2004@gamil.com", Toast.LENGTH_SHORT).show()
-               true
-               }
-           R.id.contact -> {
-               Toast.makeText(this, "+91 9084609780", Toast.LENGTH_SHORT).show()
-               true
-               }
-           R.id.feedback -> {
-               Toast.makeText(this, "Feedback", Toast.LENGTH_SHORT).show()
-               true
-           }
+            R.id.email -> {
+                intent = Intent(Intent.ACTION_SENDTO)
+                intent.data = Uri.parse("mailto:anmolmadhav2004@gamil.com")
+                startActivity(intent)
+                Toast.makeText(this, "anmolmadhav2004@gamil.com", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.contact -> {
+                intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse("tel:9084609780")
+                startActivity(intent)
+                Toast.makeText(this, "+91 9084609780", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.feedback -> {
+                intent = Intent(Intent.ACTION_SENDTO)
+                intent.data = Uri.parse("smsto:+919084609780")
+                startActivity(intent)
+
+                Toast.makeText(this, "Feedback", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.logout -> {
+                AlertDialog.Builder(this)
+                    .setTitle("Exit")
+                    .setMessage("Do you really want to exit?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes") { dialog, _ ->
+                        finishAffinity()
+                    }
+                    .setNegativeButton("No") { dialog, _ ->dialog.dismiss()
+                    }
+                    .show()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
-
     }
 }
